68ca590c3e91bea78abd7e745b14255f08c2982b
Ian Davies <id451@hotmail.com>
2017-11-12 10:58:40 +0200

    parse csv to map

--- src/accntr/core.clj
+++ src/accntr/core.clj
@@ -1,7 +1,23 @@
 (ns accntr.core
   (:gen-class))
 
+(require
+  '[clojure.data.csv :as csv]
+  '[clojure.java.io :as io])
+
+(defn csv-data->maps [csv-data]
+  (map zipmap
+    (->> (first csv-data) ;; First row is the header
+         (map keyword) ;; Drop if you want string keys instead
+         repeat)
+  	(rest csv-data)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Read and categorize expenses"
   [& args]
-  (println "Hello, World!"))
+  (with-open [reader (io/reader (first args))]
+    (doall (->>
+      (csv/read-csv reader)
+      csv-data->maps
+      (map (fn [csv-record]
+             (println csv-record)))))))
