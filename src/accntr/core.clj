eddf4d9ebb8d27a7dbe7d513df748de0f9c35e14
Ian Davies <id451@hotmail.com>
2017-11-21 20:11:46 +0200

    finished before month breakdown

--- src/accntr/core.clj
+++ src/accntr/core.clj
@@ -10,18 +10,20 @@
   [#"^PnP Fam Welge.*$" :picknpay [:food]]
   [#"^PANORAMA VETE.*$" :panorama-vet [:dogs]]
   [#"^ITUNES\.COM.*$" :itunes [:entertainment]]
-  [#"^UBER SA [help|HELP].*$" :uber [:taxi]]
+  [#"^UBER SA [Hh][Ee][Ll][Pp].*$" :uber [:taxi]]
   [#"^MWEB IN   INTERNET.*$" :mweb [:internet]]
   [#"^272713422 VOD PREPA.*$" :vodacom [:phone]]
-  [#"^VOD PREPAID    0791716430.*$" :vodacom [:phone]]
+  [#"^VOD PREPAID.*$" :vodacom [:phone]]
   [#"^PAM GOLDING.*$" :pam-golding [:rent]]
   [#"^I SnapScan Ecc.*$" :ecco [:food :work-food]]
   [#"^SUPERSPAR WELG.*$" :spar [:food]]
   [#"^I SnapScan Del.*$" :deluxe [:food :coffee]]
   [#"^ABSOLUTE PETS.*$" :absolute-pets [:dog-food]]
   [#"^VOD PREPAID    0713369497.*$" :vodacom [:phone]]
-  [#"^Clicks Gardens.*$" :clicks [:medication]]
+  [#"^Clicks.*$" :clicks [:medical]]
   [#"^FIXED MONTHLY FEE 272713422.*$" :standard-bank [:bank-fees]]
+
+  [#"^ACC   272713422.*$" :other-bank [:bank-fees]]
   [#"^ELEC           01023614165.*$" :eskom [:electricity]]
   [#"^DIS-CHEM [TYGER|TABS].*$" :dis-chem [:toiletries]]
   [#"^MONIQUE.*$" :monique [:baby-sitting]]
@@ -30,24 +32,27 @@
   [#"^272713422 ELEC      01023614165.*$" :dis-chem [:toiletries]]
   [#"^OUMEUL WILABS.*$" :ou-meul [:food :eating-out]]
   [#"^PNP KENRIDGE.*$" :picknpay [:food]]
-  [#"^Clicks Gar.*$" :clicks [:medication]]
-  [#"^I SnapScan Kne.*$" :knead [:food :work-food]]
+  [#"^Clicks Gar.*$" :clicks [:medical]]
+  [#"^I SnapScan Kne.*$" :knead [:work-food]]
+  [#"^I SnapScan Div.*$" :diva [:work-food]]
   [#"^'-387-6-1.*$" :savings [:save]]
   [#"^AIB.*$" :santam [:insurance]]
   [#"^NANDOS - BNDB.*$" :nandos [:food :eating-out]]
   [#"^T SMUTS.*$" :tanya-smuts [:psychology]]
   [#"^CLAIRE GREGOROWSKI.*$" :claire-gregorowski [:psychology]]
-  [#"^OUDEHOF APTEEK.*$" :pharmacy [:medication]]
+  [#"^OUDEHOF APTEEK.*$" :pharmacy [:medical]]
   [#"^CHAMELEONS.*$" :chameleons [:school]]
   [#"^517374 WELGEMO.*$" :bp [:petrol]]
   [#"^TOTALSPORTS.*$" :totalsports [:clothes]]
   [#"^GELATO MANIA.*$" :gelato-mania [:food :eating-out]]
   [#"^HARBOUR HOUSE.*$" :harbour-house [:food :eating-out]]
   [#"^KENRIDGE 5838.*$" :atm [:cash]]
+  [#"^ABSA.*$" :atm [:cash]]
+
   [#"^KFC.*$" :kfc [:food :eating-out]]
   [#"^C*BP KENRIDGE.*$" :bp [:petrol]]
   [#"^Dogs Bollo.*$" :dogs-bollocks [:food :work-food]]
-  [#"^BEN WEI SUSHI.*$" :ben-wei [:food :work-food]]
+  [#"^BEN WEI SU.*$" :ben-wei [:food :work-food]]
   [#"^.*WOLFPACK RUN.*$" :run-specialist-store [:exercise]]
   [#"^NIKE V&A CAPE.*$" :nike [:exercise]]
   [#"^SIMPLY ASIA.*$" :simply-asia [:food :eating-out]]
@@ -64,7 +69,7 @@
   [#"^PAUL GRAPENDAAL.*$" :paul [:other]]
   [#"^COTTON ON KIDS.*$" :cotton-on-kids [:clothes]]
   [#"^.*CAFE ROUSSE.*$" :cafe-rousse [:food :eating-out]]
-  [#"^WELLNESS.*$" :wellness-center [:medication]]
+  [#"^WELLNESS.*$" :wellness-center [:medical]]
   [#"^PEARLYS.*$" :pearlys [:food :eating-out]]
   [#"^DISC PREM.*$" :discovery [:exercise]]
   [#"^.*SCRATCH PATC.*$" :scratch-patch [:entertainment]]
@@ -83,9 +88,146 @@
   [#"^SUPERDRY.*$" :superdry [:clothes]]
   [#"^Musica.*$" :musica [:entertainment]]
   [#"^.*Snap[Ss]can.*$" :snapscan [:snapscan]]
-  [#"^THE WATERFRONT.*$" :waterfront [:other]]
+  [#"^.*WATERFRON.*$" :waterfront [:other]]
   [#"^C\*Truworths.*$" :truworths [:clothes]]
   [#"^SBSA VAF.*$" :standard-bank [:cars]]
+  [#"^UMI RESTAU.*$" :umi [:eating-out]]
+  [#"^VIRGIN ACT.*$" :virgin-active [:exercise]]
+  [#"^BILLABONG.*$" :billabong [:clothes]]
+  [#"^MOTORPORT AUTO.*$" :bp [:petrol]]
+  [#"^DIVA RESTA.*$" :diva [:work-food]]
+  [#"^PnP Crp.*$" :picknpay [:food]]
+  [#"^BONNIE.*$" :sam [:other]]
+  [#"^KAUAI JUICE.*$" :kauai [:eating-out]]
+  [#"^Truth Cof.*$" :truth [:coffee]]
+  [#"^DR C TOMC.*$" :dr-christine-tomcheck [:medical]]
+  [#"^PNP FAMILY.*$" :picknpay [:food]]
+  [#"^PNP WILLOW.*$" :picknpay [:food]]
+  [#"^STER KINE.*$" :ster-kinefor [:entertainment]]
+  [#"^PATHCARE.*$" :pathcare [:medical]]
+  [#"^TOY PLANET.*$" :toy-planet [:toys]]
+  [#"^M-KEM MEDI.*$" :m-kem [:medical]]
+  [#"^MELANIE BURGER.*$" :melanie-burger [:exercise]]
+  [#"^ENGEN.*$" :engen [:petrol]]
+  [#"^Save.*$" :save [:savings]]
+  [#"^TYGERBERG VETS.*$" :tygerberg-vets [:dogs]]
+  [#"^TABLES RESTAUR.*$" :tables [:eating-out]]
+  [#"^COLCACCHIO.*$" :colcaccios [:eating-out]]
+  [#"^COLCACHIO.*$" :colcaccios [:eating-out]]
+  [#"^ZONNEBLOEM.*$" :bp [:petrol]]
+  [#"^KNEAD.*$" :knead [:work-food]]
+  [#"^HOUT BAY VET.*$" :hout-bay-vet [:dogs]]
+  [#"^063643 WES.*$" :unknown [:other]]
+  [#"^CARLUCCIS.*$" :carluccios [:eating-out]]
+  [#"^CAFE ROUX.*$" :cafe-roux [:eating-out]]
+  [#"^LEKKER.*$" :lekker [:eating-out]]
+  [#"^CRAZY STORE.*$" :crazy-store [:other]]
+  [#"^PNP CLOTHING.*$" :picknpay [:clothes]]
+  [#"^PICK N PAY.*$" :picknpay [:food]]
+  [#"^HARTLIEF.*$" :hartlief [:work-food]]
+  [#"^SWEDO CARS.*$" :swedo-cars [:car-maintenance]]
+  [#"^.*SWATCH.*$" :swatch [:clothes]]
+  [#"^WLMD Mother an.*$" :mother [:other]]
+  [#"^SEAT.*$" :seat-meet-eat [:eating-out]]
+  [#"^QUAY 4.*$" :quay-4 [:eating-out]]
+  [#"^JD V A.*$" :jd [:other]]
+  [#"^I LOVE WAFFLES.*$" :i-love-waffles [:eating-out]]
+  [#"^Deluxe Buitenk.*$" :deluxe [:coffee]]
+  [#"^SUMMERHOUSE.*$" :summerhouse [:other]]
+  [#"^WELGEMOE 6081.*$" :atm [:cash]]
+  [#"^STOPFORTHS ATTORNEYS.*$" :stopforths [:ignore]]
+  [#"^GAME.*$" :game [:other]]
+  [#"^078723876001.*$" :transfer [:ignore]]
+  [#"^KIM AND EMILY.*$" :kim-and-emily [:other]]
+  [#"^Spar Welgedach.*$" :spar [:food]]
+  [#"^KITE LAB.*$" :kite-lab [:kitesurfing]]
+  [#"^NANDOS.*$" :nandos [:eating-out]]
+  [#"^CHRIS WILLEMSE.*$" :cwc [:exercise]]
+  [#"^MELTRADE.*$" :metrade [:entertainment]]
+  [#"^THE HART.*$" :the-hart [:eating-out]]
+  [#"^SPIROS.*$" :spiros [:other]]
+  [#"^BOOTLEGGER.*$" :bootlegger [:eating-out]]
+  [#"^DIAL 4 GAS.*$" :dial-4-gas [:electricity]]
+  [#"^517374 WEL.*$" :bp [:petrol]]
+  [#"^GSP RESCUE.*$" :gsp-rescue [:dogs]]
+  [#"^LOOKOUT DEAUTO.*$" :bp [:petrol]]
+  [#"^SUNNINGDALE CO.*$" :bp [:petrol]]
+  [#"^PLATINUM HAIR.*$" :platinum-hair [:hair]]
+  [#"^Gardens Centre.*$" :gardens-center [:parking]]
+  [#"^Interpark.*$" :interpark [:parking]]
+  [#"^ABOUT CATS.*$" :about-cats [:dogs]]
+  [#"^WEST COAST.*$" :bp [:petrol]]
+  [#"^TYGERVAL.*$" :atm [:cash]]
+  [#"^(Spar St He|SPAR ST HE).*$" :spar [:food]]
+  [#"^Spar Oakhurst.*$" :spar [:food]]
+  [#"^Cltx Melkbos.*$" :caltex [:petrol]]
+  [#"^.*FEE.*-.*OTHER BANK.*$" :other-bank [:bank-fees]]
+  [#"^SACRED GROUND.*$" :sacred-ground [:eating-out]]
+  [#"^VASA Tygervall.*$" :vasa-tygervalley [:unknown]]
+  [#"^Builders Exp.*$" :builders-express [:other]]
+  [#"^BELTHAZAR REST.*$" :belthazar [:eating-out]]
+  [#"^MASSIMOS.*$" :massimos [:eating-out]]
+  [#"^LIMNOS.*$" :limnos [:eating-out]]
+  [#"^SANBI KIRSTEN.*$" :san-parks [:entertainment]]
+  [#"^TMNP BOULDERS.*$" :san-parks [:entertainment]]
+  [#"^DR M KUHN.*$" :dr-m-kuhn [:medical]]
+  [#"^BABY BOOM WILL.*$" :baby-boom [:toys]]
+  [#"^THE BLUE.*$" :the-blue [:eating-out]]
+  [#"^NEW YORK.*$" :new-york [:eating-out]]
+  [#"^MELONCINO.*$" :meloncino [:eating-out]]
+  [#"^PNP TYGERVALLE.*$" :picknpay [:food]]
+  [#"^WELGEDACHT TOP.*$" :tops [:food]]
+  [#"^SORBET.*$" :sorbet [:eating-out]]
+  [#"^.*SPUR.*$" :spur [:eating-out]]
+  [#"^TYGERBERG ABS.*$" :absolute-pets [:dogs]]
+  [#"^.*CACCHIO.*$" :colcaccios [:eating-out]]
+  [#"^Spar Welge.*$" :spar [:food]]
+  [#"^FOSSIL.*$" :fossil [:clothes]]
+  [#"^HAAGEN DAZ.*$" :haagen-dazs [:eating-out]]
+  [#"^ROYALE EAT.*$" :royale-eatery [:work-food]]
+  [#"^CHARLYS B.*$" :charlys-bakery [:work-food]]
+  [#"^SENTINEL SABS.*$" :bp [:petrol]]
+  [#"^PET TRAVEL.*$" :pet-travel [:dogs]]
+  [#"^OXFORD STAMLB .*$" :oxford-stam [:other]]
+  [#"^CASSIA RESNDB .*$" :cassia [:eating-out]]
+  [#"^FAMOUS K510000.*$" :famous [:eating-out]]
+  [#"^MAINSTRE 0741 .*$" :atm [:cash]]
+  [#"^STAR DOT TOYS .*$" :star-dot-toys [:toys]]
+  [#"^Furniture     .*$" :furniture [:other]]
+  [#"^CONSTANTIABERG.*$" :constantiaberg-mediclinic [:medical]]
+  [#"^GUYS SERVICE C.*$" :guys-service [:other]]
+  [#"^.*STEERS.*$" :steers [:eating-out]]
+  [#"^KLOOF STRE1NB .*$" :kloof-street [:other]]
+  [#"^DAMHUIS BECTB .*$" :damhuis [:eating-out]]
+  [#"^DION WIRED TYG.*$" :dion-wired [:other]]
+  [#"^CAFEEN    AUTO.*$" :cafeen [:eating-out]]
+  [#"^PANCHO S  AUTO.*$" :pancho [:eating-out]]
+  [#"^BASH VENUE    .*$" :bash-venue [:entertainment]]
+  [#"^TYGER MA 5749 .*$" :atm [:cash]]
+  [#"^BATTLE BUN1NB .*$" :battle-bunker [:entertainment]]
+  [#"^JERRY\'S WIAUTO.*$" :jerrys-willowbrige [:eating-out]]
+  [#"^THE ALPHEN LA .*$" :the-alphen [:eating-out]]
+  [#"^ALLEGRIA CAFE .*$" :allegria [:eating-out]]
+  [#"^LORRAINE PEDER.*$" :lorraine-pederson [:entertainment]]
+  [#"^EAT AT ALTMLB .*$" :eat-at-alt [:eating-out]]
+  [#"^.*THE GREEK ME.*$" :the-greek [:eating-out]]
+  [#"^CROWN RESTABS .*$" :crown-restaurant [:eating-out]]
+  [#"^BORRUSOS PABS .*$" :borrusos [:eating-out]]
+  [#"^COPPERFIELD CH.*$" :copperfield [:other]]
+  [#"^VOVO TELO NDB .*$" :vovo-telo [:eating-out]]
+  [#"^.*SPORTSMANS W.*$" :sportsmans-warehouse [:exercise]]
+  [#"^CNA TYGERVALLE.*$" :cna [:other]]
+  [#"^.*CREATION WIN.*$" :creation-wines [:eating-out]]
+  [#"^TWO OCEANSNDB .*$" :two-oceans [:eating-out]]
+  [#"^.*YDE 0813    .*$" :yde [:clothes]]
+  [#"^NEDCOR.*$" :atm [:cash]]
+  [#"^PINE GARDE.*$" :pine-garden [:eating-out]]    
+  [#"^MONT MARIE.*$" :mont-marie [:eating-out]]
+  [#"^CORICRAFT.*$" :coricraft [:other]]
+  [#"^KE-MONATE REST.*$" :ke-monate [:eating-out]]
+  [#"^VODACOM SERVIC.*$" :vodacom [:phone]]
+  [#"^DR J LA GRANGE.*$" :dr-j-la-grange [:medical]]
+  [#"^SUPER SPAR AUR.*$" :spar [:food]]
   ;[#"^.*$" : [:]]
   ;[#"^.*$" : [:]]
   ;[#"^.*$" : [:]]
@@ -102,6 +244,25 @@
   ;[#"^.*$" : [:]]
   ;[#"^.*$" : [:]]
   ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+  ;[#"^.*$" : [:]]
+
 ])
 
 (defn csv-data->maps [csv-data]
@@ -111,27 +272,26 @@
          repeat)
   	(rest csv-data)))
 
-(defn first-match [csv-record]
+(defn first-match [csv-record tag]
   (first
     (filter
-      (fn [category]
-        (let [
-          match (re-matches (first category) (:description csv-record))]
-          match))
+      (fn [category] (re-matches (first category) (tag csv-record)))
       categorization)))
+
+(defn category-to-object [csv-record category] {
+  :supplier (second category)
+  :tags (last category)
+  :amount (Double/parseDouble (:amount csv-record))
+  :record csv-record})
 
 (defn categorize [csv-data->maps]
   (map (fn [csv-record]
-    (let [
-        category (first-match csv-record)
-        categorized {
-            :supplier (second category)
-            :tags (last category)
-            :amount (Double/parseDouble (:amount csv-record))
-            :record csv-record
-          }]
-      categorized)
-      ) csv-data->maps))
+    (category-to-object csv-record
+      (if-let [description-match (first-match csv-record :description)]
+        description-match
+        (if-let [source-match (first-match csv-record :source)]
+            source-match))))
+    csv-data->maps))
 
 (defn parse-all [filename]
   (with-open [reader (io/reader filename)]
@@ -147,7 +307,10 @@
   (filter (complement (fn [row] (some #(= tag %) (:tags row)))) categorized))
 
 (defn calculate [description categorized]
-  (let [amount (reduce + (map #(:amount %) categorized))]
+  (let [amount
+      (reduce + (map
+        (fn [c]
+          (:amount c)) categorized))]
     (println description amount)
     amount))
 
@@ -161,13 +324,12 @@
     uncategorized-debits (filter #(>= 0 (:amount %)) uncategorized)
     ]
     (doseq [record (map #(:record %) uncategorized-debits)]
-      (println (:description record) (:source record) (:amount record)))
+      (println (:description record) ": " (:source record) (:amount record) (:date record)))
     (println (count uncategorized-debits)
     (let [total
         (+
           (calculate "Rent" (search :rent categorized))
           (calculate "Cars" (search :cars categorized))
-          ;(calculate "Save" (search :save categorized))
           (calculate "Food"
             (search-not :coffee
               (search-not :work-food
@@ -179,6 +341,8 @@
           (calculate "Dogs" (search :dogs categorized))
           (calculate "Taxi" (search :taxi categorized))
           (calculate "Petrol" (search :petrol categorized))
+          (calculate "Car Maintenance" (search :car-maintenance categorized))
+          (calculate "Parking" (search :parking categorized))
           (calculate "Exercise" (search :exercise categorized))
           (calculate "Clothes" (search :clothes categorized))
           (calculate "Kitesurfing" (search :kitesurfing categorized))
@@ -190,14 +354,18 @@
           (calculate "Toys" (search :toys categorized))
           (calculate "School" (search :school categorized))
           (calculate "Hair" (search :hair categorized))
-          (calculate "Medication" (search :medication categorized))
+          (calculate "Medical" (search :medical categorized))
           (calculate "Psychology" (search :psychology categorized))
           (calculate "Toiletries" (search :toiletries categorized))
           (calculate "Babysitting" (search :baby-sitting categorized))
           (calculate "Bank Fees" (search :bank-fees categorized))
           (calculate "Internet" (search :internet categorized))
           (calculate "Insurance" (search :insurance categorized))
-          (calculate "Coffee" (search :coffee categorized))
-          (calculate "Other" (search :other categorized)))]
+          (calculate "Coffee" (search-not :food (search :coffee categorized)))
+          (calculate "Toys" (search :toys categorized))
+          (calculate "Unknown" (search :unknown categorized))
+          (calculate "Other" (search :other categorized))
+          )
+          ]
         (println total)
     ))))
